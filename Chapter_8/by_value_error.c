43157d2e4dffaa10726e70778ba17e2b482b89b2
s1705159 <s1705159@sms.ed.ac.uk>
2021-04-03 19:14:43 +0100

    Add new folder for Chapter 8 of the Effective C book.

---
+++ Chapter_8/by_value_error.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    FILE my_stdout = *stdout;
+    if (fputs("Hello, World!\n", &my_stdout) == EOF) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
